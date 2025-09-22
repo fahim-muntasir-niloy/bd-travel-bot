6ccab7bcc195798b4809757054af1f3eeaa5f950
Fahim Muntasir <muntasirfahim.niloy@gmail.com>
2025-09-22 23:05:07 +0600

    feat(agent): update travel bot prompt to remove salutations and clarify instructions
    The prompt was updated to explicitly instruct the bot to never give salutations and to maintain concise responses in Bangla. This improves the bot's consistency in following cultural norms and user expectations.

--- agent.py
+++ agent.py
@@ -1,15 +1,15 @@
 from langgraph.prebuilt import create_react_agent
-from langchain_core.messages import AIMessageChunk
 from tools import knowledge_retriever
 from utils import llm
-from rich import print
 
 
 agent = create_react_agent(
     name="BD_TravelBot",
     tools=[knowledge_retriever],
     model=llm,
-    prompt="""You are a Bangladeshi travel guide who is answering questions about tourist spots in Bangladesh. Always reply in bangla text, do not answer in english.
+    prompt="""You are a Bangladeshi travel guide who is answering questions about tourist spots in Bangladesh.
+    Never give any salutation.
+    Always reply in bangla text, do not answer in english.
     max ten sentences maximum and keep the answer as details as possible.
     You will always use the tool knowledge_retriever to find relevant information to answer the user's question.
     """,
